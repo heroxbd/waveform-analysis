568500a3a64bb053d58ccbafbbc376dcba3c1b50
xudachengTHU <xudachengthu@hotmail.com>
2019-11-27 15:14:17 +0800

    Prepare for L_BFGS_B

--- test/xiaopeip/finalfit.py
+++ test/xiaopeip/finalfit.py
@@ -31,22 +31,22 @@ def main(fopt, fipt, aver_spe_path):
         for i in range(l):
             wf_input = ent[i]['Waveform']
             wave = wf_input - 972 - aver
-            lowp = np.argwhere(wave < -6.5)
+            lowp = np.argwhere(wave < -6.5).flatten()
             if len(lowp) != 0:
                 if lowp[0] < 1:
                     lowp = lowp[1:]
                 if lowp[-1] >= 1028:
                     lowp = lowp[:-1]
                 panel = np.zeros(1029)
-                ddiff = np.zeros(len(lowp))
                 for j in lowp:
                     head = j-7 if j-7 > 0 else 0
                     tail = j+15+1 if j+15+1 <= 1029 else 1029
                     panel[head:tail] = 1
-                    ddiff[j] = wave[j+1]-wave[j]-wave[j-1]+wave[j-2]
-                nihep = np.argwhere(panel == 1)
-                xuhao = lowp[ddiff > 1.5]
+                nihep = np.argwhere(panel == 1).flatten()
+                xuhao = np.argwhere(wave[lowp+1]-wave[lowp]-wave[lowp-1]+wave[lowp-2] > 1.5).flatten()
                 possible = np.unique(np.concatenate((lowp[xuhao]-10,lowp[xuhao]-9,lowp[xuhao]-8)))
+            else:
+                
         print('\rAnsw Generating:|{}>{}|{:6.2f}%'.format(((20*i)//l)*'-', (19-(20*i)//l)*' ', 100 * ((i+1) / l)), end='' if i != l-1 else '\n') # show process bar
         end_t = time.time()
     return 
