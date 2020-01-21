7004dd1803de2b673e95b165c993c47c2e062b3a
littlewu2508 <xgreenlandforwyy@gmail.com>
2020-01-21 23:19:25 +0800

    finish Cuda_Queue.py

--- test/tara/Cuda_Queue.py
+++ test/tara/Cuda_Queue.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+import time
 
 import pynvml
 pynvml.nvmlInit()
@@ -11,20 +12,20 @@ def check_available(gpu_id,min_memory):
     cuda_handle = pynvml.nvmlDeviceGetHandleByIndex(gpu_id)
     meminfo = pynvml.nvmlDeviceGetMemoryInfo(cuda_handle)
     mem = meminfo.free
-    utilrate = pynvml.nvmlDeviceGetUtilizationRates.gpu
+    utilrate = pynvml.nvmlDeviceGetUtilizationRates(cuda_handle).gpu
     if utilrate<90 and mem>min_memory :
         return True
     else :
         return False
 
-def read_bullentin(fileno) :
+def QueueUp(fileno) :
     with open("./.bulletin.swp",'r') as fi:
         lines=fi.readlines()
     try :
         File_Numbers = np.array(lines[0].split(),dtype=np.int)
         last_run_info = np.array(lines[-1].split(),dtype=np.int)
     except ValueError :
-        raise ValueError("unknown FileNo!")
+        raise FileExistsError("Wrong Format .bullentin.swp")
 
     runNo = np.where(File_Numbers==fileno)[0]
     if len(runNo)==0 : 
@@ -36,15 +37,13 @@ def read_bullentin(fileno) :
         if len(lines)!=1 :
             raise FileExistsError("Wrong Format .bullentin.swp")
         else :
-            Proceed()
-    elif len(lines)=(runNo+1) :
+            return True
+    elif len(lines)==(runNo+1) :
         if last_run_info[0]==File_Numbers[runNo-1] :
-            if last_run_info[1]==1 :
-                Proceed()
+            if last_run_info[1]==0 :
+                return True
             else :
-                raise RuntimeError('Load taining Data and model failed, file numer=%d'.format(File_Numbers[runNo-1]))
+                raise RuntimeError('Load taining Data and model failed, file numer={}'.format(File_Numbers[runNo-1]))
         else :
             raise FileExistsError("Wrong Format .bullentin.swp")
-
-def Proceed() :
-    return
+    return False
