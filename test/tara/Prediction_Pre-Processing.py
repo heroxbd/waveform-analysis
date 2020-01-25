e9d2bb9930eb53abe21d2d82266ff3d2a7e9ac92
littlewu2508 <xgreenlandforwyy@gmail.com>
2020-01-25 21:23:24 +0800

    save h5 file as pre-data

--- test/tara/Prediction_Pre-Processing.py
+++ test/tara/Prediction_Pre-Processing.py
@@ -1,3 +1,5 @@
+import time
+time_start = time.time()
 import tables
 import numpy as np
 import os
@@ -10,15 +12,13 @@ SavePath = sys.argv[2]
 if not os.path.exists(SavePath):
     os.makedirs(SavePath)
 # Read hdf5 file
-# LoadPath = sys.argv[3]
-# FileName = 'training'
-# FileName = 'ztraining-1'
-# filename = LoadPath+FileName+".h5"
-
 
 h5file = tables.open_file(fullfilename, "r")
 WaveformTable = h5file.root.Waveform
 Len_Entry = len(WaveformTable)
+max_set_number = int(sys.argv[3])
+if max_set_number>0 :
+    Len_Entry = min(max_set_number,Len_Entry)
 print(Len_Entry, "data entries") # Entry 10^6
 # WaveChannel 0-1028, length 600
 
@@ -39,44 +39,41 @@ ChanMat=[]
 WaveMat=[]
 save_period = 200000
 
+WindowSize = len(WaveformTable[0]['Waveform'])
+
+class PreProcessedData(tables.IsDescription):
+    EventID    = tables.Int64Col(pos=0)
+    ChannelID  = tables.Int16Col(pos=1)
+    Waveform   = tables.Col.from_type('int16', shape=windowSize, pos=2)
+#create Pre-Processed output file
+Prefile = tables.open_file(SavePath+"Pre.h5", mode="w", title="Pre-Processed-Training-Data")
+
+# Create group and tables
+group = "/"
+TestDataTable = Prefile.create_table(group, "TestDataTable", PreProcessedData, "Wave and PET")
+testdata = TestDataTable.row
 #saving params
 start_entry = 0
 entry_index = 0
 
+
 for entry in range(Num_Entry):
-    EventId = WaveformTable[entry]['EventID']
-    ChannelId = WaveformTable[entry]['ChannelID']
+    testdata["EvenID"] = WaveformTable[entry]['EventID']
+    testdata["ChannelID"] = WaveformTable[entry]['ChannelID']
     Waveform = WaveformTable[entry]['Waveform']
-    Wave_Vec = make_wave_long_vec(Waveform)
-    EventMat.append(EventId)
-    ChanMat.append(ChannelId)
-    WaveMat.append(Wave_Vec)
+    testdata["Waveform"] = make_wave_long_vec(Waveform)
 
     # periodic save to avoid memory crash
-    if (entry+1) % save_period ==0:
-        EventData = np.array(EventMat)
-        ChanData = np.array(ChanMat)
-        WaveData = np.array(WaveMat)
-        name = SavePath+'prediction_'+str(start_entry)+'-'+str(entry)
-        np.savez(name, Event=EventData, Chan=ChanData, Wave=WaveData)
-        start_entry = entry+1
-        EventMat = []
-        ChanMat = []
-        WaveMat = []
+    if (entry+1) % save_period ==0 or entry==Num_Entry:
+        TestDataTable.flush()
         print("Data Saved")
 
     # check point
     if entry%5000==0:
         print(entry)
-
-# Data Save for the final round
-EventData = np.array(EventMat)
-ChanData = np.array(ChanMat)
-WaveData = np.array(WaveMat)
-name = SavePath+'prediction_'+str(start_entry)+'-'+str(Num_Entry-1)
-np.savez(name, Event=EventData, Chan=ChanData, Wave=WaveData)
-print("Data Saved")
 print("Prediction Pre-processing Finished")
 
 h5file.close()
-
+Prefile.close()
+time_end = time.time()
+print('consuming time: {}s'.format(time_end-time_start))
