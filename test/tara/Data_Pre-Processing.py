6368f4d28e4798276942938834633ea569ee1700
littlewu2508 <xgreenlandforwyy@gmail.com>
2020-01-25 16:23:06 +0800

    Revert "move func to the end"
    This reverts commit f6e8b07da8b5b2eeddc5a6f5d3f4857d6429406b.

--- test/tara/Data_Pre-Processing.py
+++ test/tara/Data_Pre-Processing.py
@@ -23,6 +23,42 @@ GroundTruthTable = h5file.root.GroundTruth
 Len_Entry = len(WaveformTable)
 print(Len_Entry, "data entries") # Entry 10^6
 
+# Make Data Matrix
+def make_time_long_vec(time_mark_vec,WindowSize):
+    time_long_vec = np.zeros(WindowSize)
+    time_long_vec = np.zeros(WindowSize)
+    for time_mark in time_mark_vec:
+        if time_mark < WindowSize-9:
+            time_long_vec[time_mark] += 1
+    if len(time_mark_vec) == 0:
+        print("non-PET-event")
+    return np.array(time_long_vec,dtype=np.float32)
+
+def make_wave_long_vec(wave_form):
+    # non_negative_peak + zero_base_level
+    shift = np.argmax(np.bincount(wave_form)) #make baseline shifts, normally 972
+    shift = np.mean(wave_form[np.abs(wave_form-shift)<3])
+    shift_wave=np.array(wave_form-shift,dtype=np.int16)
+    #non_negative_peak + zero_base_level
+    if np.max(shift_wave) >= -np.min(shift_wave) : return shift_wave
+    if np.max(shift_wave) < -np.min(shift_wave) : return -shift_wave
+
+def search_PETime(event_id,channel_id,PETtable,starting_point):
+    PET_num = 0
+    index = starting_point
+    LEN = len(PETtable)
+    PET_total = []
+    while index < LEN and PETtable[index]['EventID'] == event_id and PETtable[index]['ChannelID'] == channel_id:
+        PET_total.append(PETtable[index]['PETime'])
+        PET_num += 1
+        if index+1 < LEN:
+            index += 1
+        else:
+            break
+    ending_point = index
+    return PET_total, ending_point
+
+
 # Data Pre-Processing
 Num_Entry = Len_Entry #100000 #Len_Entry
 
@@ -74,39 +110,3 @@ h5file.close()
 Prefile.close()
 time_end = time.time()
 print('consuming time: {}s'.format(time_end-time_start))
-# end Pre-Processing
-
-# Make Data Matrix
-def make_time_long_vec(time_mark_vec,WindowSize):
-    time_long_vec = np.zeros(WindowSize)
-    time_long_vec = np.zeros(WindowSize)
-    for time_mark in time_mark_vec:
-        if time_mark < WindowSize-9:
-            time_long_vec[time_mark] += 1
-    if len(time_mark_vec) == 0:
-        print("non-PET-event")
-    return np.array(time_long_vec,dtype=np.float32)
-
-def make_wave_long_vec(wave_form):
-    # non_negative_peak + zero_base_level
-    shift = np.argmax(np.bincount(wave_form)) #make baseline shifts, normally 972
-    shift = np.mean(wave_form[np.abs(wave_form-shift)<3])
-    shift_wave=np.array(wave_form-shift,dtype=np.int16)
-    #non_negative_peak + zero_base_level
-    if np.max(shift_wave) >= -np.min(shift_wave) : return shift_wave
-    if np.max(shift_wave) < -np.min(shift_wave) : return -shift_wave
-
-def search_PETime(event_id,channel_id,PETtable,starting_point):
-    PET_num = 0
-    index = starting_point
-    LEN = len(PETtable)
-    PET_total = []
-    while index < LEN and PETtable[index]['EventID'] == event_id and PETtable[index]['ChannelID'] == channel_id:
-        PET_total.append(PETtable[index]['PETime'])
-        PET_num += 1
-        if index+1 < LEN:
-            index += 1
-        else:
-            break
-    ending_point = index
-    return PET_total, ending_point
