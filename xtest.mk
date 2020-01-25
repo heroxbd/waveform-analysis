6d2940a379dc501c313ffaae0a78ffe7bc4b4ea6
xudacheng <xudachengthu@hotmail.com>
2020-01-25 11:12:25 +0000

    Record dist of total.h5 & submission.h5

--- xtest.mk
+++ xtest.mk
@@ -3,16 +3,24 @@ jinpDir=dataset/jinp
 xiaoPp=test/xiaopeip
 
 xtest=xtest
-xtestPl=99
+xtestPl=49
 xtestseq=$(shell seq 0 ${xtestPl})
 
-xall: $(xtest)/hist-x.pdf $(xtest)/record-x.csv
-$(xtest)/record-x.csv: $(xtest)/distrecord/distrecord-x.h5
+xall: $(xtest)/total-hist-x.pdf $(xtest)/total-record-x.csv $(xtest)/submission-hist-x.pdf $(xtest)/submission-record-x.csv
+$(xtest)/submission-record-x.csv: $(xtest)/distrecord/submission-distrecord-x.h5
 	mkdir -p $(dir $@)
 	python3 test/csv_dist.py $^ -o $@
-$(xtest)/hist-x.pdf: $(xtest)/distrecord/distrecord-x.h5
+$(xtest)/submission-hist-x.pdf: $(xtest)/distrecord/submission-distrecord-x.h5
 	python3 test/draw_dist.py $^ --wthres 20 -o $@
-$(xtest)/distrecord/distrecord-x.h5: $(xtest)/ztraining-x.h5 $(xtest)/submission/submission-x.h5
+$(xtest)/distrecord/submission-distrecord-x.h5: $(xtest)/ztraining-x.h5 $(xtest)/submission/submission-x.h5
+	mkdir -p $(dir $@)
+	python3 test/test_dist.py $(word 2,$^) --ref $< -o $@ > $@.log 2>&1
+$(xtest)/total-record-x.csv: $(xtest)/distrecord/total-distrecord-x.h5
+	mkdir -p $(dir $@)
+	python3 test/csv_dist.py $^ -o $@
+$(xtest)/total-hist-x.pdf: $(xtest)/distrecord/total-distrecord-x.h5
+	python3 test/draw_dist.py $^ --wthres 20 -o $@
+$(xtest)/distrecord/total-distrecord-x.h5: $(xtest)/ztraining-x.h5 $(xtest)/submission/total-x.h5
 	mkdir -p $(dir $@)
 	python3 test/test_dist.py $(word 2,$^) --ref $< -o $@ > $@.log 2>&1
 $(xtest)/submission/submission-x.h5: $(xtest)/submission/total-x.h5
@@ -26,6 +34,6 @@ $(xtest)/unadjusted/unadjusted-x-%.h5: $(xtest)/ztraining-x.h5 $(xiaoPp)/averspe
 	python3 $(xiaoPp)/finalfit.py $< --ref $(word 2,$^) --num ${xtestPl} -o $@
 $(xtest)/ztraining-x.h5: $(jinpDir)/ztraining-1.h5
 	mkdir -p $(dir $@)
-	python3 test/cut_data.py $^ -o $@ -a -1 -b 10000
+	python3 test/cut_data.py $^ -o $@ -a -1 -b 300
 $(xiaoPp)/averspe.h5: $(jinpDir)/ztraining-0.h5
 	python3 test/spe_get.py $^ -o $@ --num 10000 --len 80
