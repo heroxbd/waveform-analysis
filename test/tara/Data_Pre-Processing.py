84110ec2b8f3fde92136be9a53d3ecc84c6f394f
WuYiyang <wuyiyang17@mails.tsinghua.edu.cn>
2020-03-25 13:42:32 +0000

    Use JPwaptool_Lite to handle wave shift and petime

--- test/tara/Data_Pre-Processing.py
+++ test/tara/Data_Pre-Processing.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import sys
 import tables
+from JPwaptool_Lite import JPwaptool_Lite
 
 ## Initialization
 
@@ -26,64 +27,30 @@ max_set_number = int(sys.argv[3])
 if max_set_number>0 :
     Len_Entry = min(max_set_number,Len_Entry)
 
+if(Len_Entry<=100) : raise ValueError("max_set_number must > 100 !")
+
 print(Len_Entry, "data entries") # Entry 10^6
 
 # Make Data Matrix
-def make_time_long_vec(time_mark_vec,WindowSize):
-    time_long_vec = np.zeros(WindowSize)
-    time_long_vec = np.zeros(WindowSize)
-    for time_mark in time_mark_vec:
-        if time_mark < WindowSize-9:
-            time_long_vec[time_mark] += 1
-    if len(time_mark_vec) == 0:
-        print("non-PET-event")
-    return np.array(time_long_vec,dtype=np.float32)
+Waveforms_and_info = WaveformTable[0:Len_Entry]
+example_wave = Waveforms_and_info["Waveform"][0:100]
+WindowSize = len(example_wave[0])
+is_positive_pulse = (example_wave.max(axis=1)-example_wave.mean(axis=1))>(example_wave.mean(axis=1)-example_wave.min(axis=1))
+Waveforms = Waveforms_and_info["Waveform"]
+if sum(is_positive_pulse)>95 : #positive pulse
+    Waveforms = Waveforms.max()-Waveforms
+elif sum(is_positive_pulse)>5 : raise ValueError("ambiguous pulse!")
 
-def make_wave_long_vec(wave_form):
-    # non_negative_peak + zero_base_level
-    shift = np.argmax(np.bincount(wave_form)) #make baseline shifts, normally 972
-    shift = np.mean(wave_form[np.abs(wave_form-shift)<3])
-    shift_wave=np.array(wave_form-shift,dtype=np.int16)
-    #non_negative_peak + zero_base_level
-    if np.max(shift_wave) >= -np.min(shift_wave) : return shift_wave
-    if np.max(shift_wave) < -np.min(shift_wave) : return -shift_wave
-
-def search_PETime(event_id,channel_id,PETtable,starting_point):
-    PET_num = 0
-    index = starting_point
-    LEN = len(PETtable)
-    PET_total = []
-    while index < LEN and PETtable[index]['EventID'] == event_id and PETtable[index]['ChannelID'] == channel_id:
-        PET_total.append(PETtable[index]['PETime'])
-        PET_num += 1
-        if index+1 < LEN:
-            index += 1
-        else:
-            break
-    ending_point = index
-    return PET_total, ending_point
-
-
-# Data Pre-Processing
-Num_Entry = Len_Entry #100000 #Len_Entry
-
-
-save_period = 200000
-looking_up_index = 0
-
-#saving params
-start_entry = 0
-
-EventMat=[]
-ChanMat=[]
-WaveMat=[]
-PETMat=[]
-
-WindowSize = len(WaveformTable[0]['Waveform'])
+if WindowSize>=1000 :
+    stream = JPwaptool_Lite(WindowSize,100,600)
+elif WindowSize==600 :
+    stream = JPwaptool_Lite(WindowSize,50,400)
+else : 
+    raise ValueError("Unknown WindowSize, I don't know how to choose the parameters for pedestal calculatation")
 
 class PreProcessedData(tables.IsDescription):
-    PET = tables.Col.from_type('int16', shape=WindowSize, pos=0)
-    Wave = tables.Col.from_type('int16', shape=WindowSize, pos=1)
+    PET = tables.Col.from_type('float32', shape=WindowSize, pos=0)
+    Wave = tables.Col.from_type('float32', shape=WindowSize, pos=1)
     
 #create Pre-Processed output file
 Prefile = tables.open_file(SavePath+"Pre.h5", mode="w", title="Pre-Processed-Training-Data")
@@ -93,24 +60,22 @@ group = "/"
 TrainDataTable = Prefile.create_table(group, "TrainDataTable", PreProcessedData, "Wave and PET")
 traindata = TrainDataTable.row
 
-for entry in range(Num_Entry):
-    EventId = WaveformTable[entry]['EventID']
-    ChannelId = WaveformTable[entry]['ChannelID']
-    Waveform = WaveformTable[entry]['Waveform']
-    PETime, looking_up_index = search_PETime(EventId,ChannelId,GroundTruthTable,looking_up_index)
-    traindata['PET'] = make_time_long_vec(np.array(PETime,dtype=np.int16),WindowSize)
-    traindata['Wave'] = make_wave_long_vec(Waveform)
-    traindata.append()
+GroundTruth_Len = min(round(len(GroundTruthTable)/len(WaveformTable)*Len_Entry*2),len(GroundTruthTable))
+last_eventid = Waveforms_and_info["EventID"][-1]
+last_channelid = Waveforms_and_info["ChannelID"][-1]
+GroundTruth = GroundTruthTable[0:GroundTruth_Len]
+TimeSeries = stream.Make_Time_Vector(GroundTruth["EventID"],GroundTruth["ChannelID"],GroundTruth["PETime"],np.int64(last_eventid),np.int16(last_channelid),np.int64(Len_Entry))
 
-    # periodic save to avoid memory crash
-    if (entry+1) % save_period ==0 or entry==Num_Entry:
-        TrainDataTable.flush()
-        print("Data Saved")
+for entry in range(Len_Entry):
+    stream.Calculate(Waveforms[entry])
+    traindata['Wave'] = stream.ChannelInfo.Ped - Waveforms[entry] 
+    traindata['PET'] = TimeSeries[entry]
+    traindata.append()
 
     # check point
     if (entry+1)%5000==0:
         print(entry+1)
-
+TrainDataTable.flush()
 h5file.close()
 Prefile.close()
 time_end = time.time()
