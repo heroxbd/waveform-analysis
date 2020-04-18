d41894f05a3515f33391e2fa7f3186444eafbfbb
WuYiyang <wuyiyang17@mails.tsinghua.edu.cn>
2020-04-18 00:39:55 +0000

    Finished pipeline of seperated channel Prediction

--- test/tara/Prediction_Processing_Total.py
+++ test/tara/Prediction_Processing_Total.py
@@ -7,12 +7,12 @@ psr.add_argument('NetDir', help='Network directory')
 psr.add_argument('-B', '--batchsize', dest='BAT', type=int, default=15000)
 args = psr.parse_args()
 NetDir = args.NetDir
-outputs = args.opt
+output = args.opt
 filename = args.ipt
 BATCHSIZE = args.BAT
 
 import time
-from IPython import embed  # ipython breakpoint inserting
+global_start = time.time()
 import numpy as np
 import tables
 import pandas as pd
@@ -21,7 +21,7 @@ import pandas as pd
 import torch
 from torch.nn import functional as F
 
-from multiprocessing import Pool, Process, Pipe, Lock
+from multiprocessing import Pool
 from JPwaptool_Lite import JPwaptool_Lite
 
 # Loading Data
@@ -61,33 +61,32 @@ else :
     ranges = list(zip(slices[0:-1], slices[1:]))
     with Pool(N) as pool :
         Waveforms_and_info = pd.concat(pool.starmap(Read_Data, ranges))
-print(N, end=': ')
-print(time.time() - tic)
-
+print("Data Loaded, consuming {0:.4f} using {1} threads".format(time.time() - tic, N))
 
 channelid_set = set(Waveforms_and_info['ChannelID'])
 Channel_Grouped_Waveform = Waveforms_and_info.groupby(by="ChannelID")
 
-
+# Loading CNN Net
 device = torch.device(1)
+nets = dict([])
+for channelid in channelid_set :
+    nets[channelid] = torch.load(NetDir + "/Channel{}.torch_net".format(channelid), map_location=device)
+
 filter_limit = 0.9 / WindowSize
 Timeline = torch.arange(WindowSize, device=device)
 
 
 def Forward(channelid) :
-    net = torch.load(NetDir + "/Channel{}.torch_net".format(channelid), map_location=device)  # Pre-trained Model Parameters
-    start = time.time()
     Data_of_this_channel = Channel_Grouped_Waveform.get_group(channelid)
     Shifted_Wave = np.vstack(Data_of_this_channel['Waveform'])
     EventIDs = np.array(Data_of_this_channel['EventID'])
-    print("channel {0} Received, consuming {1:.4f}".format(ch, time.time() - start))
     PETimes = np.empty(0, dtype=np.int16)
     Weights = np.empty(0, dtype=np.float32)
     EventData = np.empty(0, dtype=np.int64)
     slices = np.append(np.arange(0, len(Shifted_Wave), BATCHSIZE,), len(Shifted_Wave))
     for i in range(len(slices) - 1) :
         inputs = torch.from_numpy(Shifted_Wave[slices[i]:slices[i + 1]])
-        Prediction = net.forward(inputs.to(device=device)).data
+        Prediction = nets[channelid].forward(inputs.to(device=device)).data
         PETime = Prediction > filter_limit
         pe_numbers = PETime.sum(1)
         no_pe_found = (pe_numbers == 0)
@@ -111,93 +110,20 @@ Result = []
 for ch in channelid_set :
     Result.append(Forward(ch))
 Result = pd.concat(Result)
-embed()
+Result.sort_values(by=["EventID", "ChannelID"])
 
-#parent_conn1, child_conn1 = Pipe()
-#parent_conn2, child_conn2 = Pipe()
-#p1 = Process(target=Prepare, args=(child_conn1, 0))
-# p1.start()
-#p2 = Process(target=Forward, args=(child_conn2, parent_conn1.recv(), 0))
-# p2.start()
-#print("p2 start")
-# p1.join()
-#print("p1 finished")
-# p2.join()
-#print("p2 finished")
-# print(parent_conn2.recv())
-# with Pool(2) as pool :
-#    Results = pool.map(Forward,range(30))
-#
-#
-# Data Settings
-#LoadingPeriod = 20000
-# h5 file handling
-# Define the database columns
-#
-#
-# class AnswerData(tables.IsDescription):
-#    EventID = tables.Int64Col(pos=0)
-#    ChannelID = tables.Int16Col(pos=1)
-#    PETime = tables.Int16Col(pos=2)
-#    Weight = tables.Float32Col(pos=3)
-#
-#
-# Create the output file and the group
-## h5file = tables.open_file("./Prediction_Results/Prediction_Mod_ztraining.h5", mode="w", title="OneTonDetector")
-#h5file = tables.open_file(outputs, mode="w", title="OneTonDetector", filters=tables.Filters(complevel=9))
-#
-# Create tables
-#AnswerTable = h5file.create_table("/", "Answer", AnswerData, "Answer")
-#answer = AnswerTable.row
-#
-#
-# Prepare for data and generating answer from prediction
-#entryList = np.arange(0, Total_entries, LoadingPeriod)
-#entryList = np.append(entryList, Total_entries)
-#start_time = time.time()
-#
-#
-# Loop for batched data
-# for k in range(len(entryList) - 1) :
-#    # Making Dataset
-#    EventData = Data_set[entryList[k]:entryList[k + 1]]['EventID']
-#    ChanData = Data_set[entryList[k]:entryList[k + 1]]['ChannelID']
-#    WaveData = Data_set[entryList[k]:entryList[k + 1]]['Waveform']
-#    inputs = torch.empty((len(WaveData), WindowSize), device=device)
-#    for i in range(len(WaveData)) :
-#        stream.Calculate(WaveData[i])
-#        inputs[i] = torch.from_numpy(stream.ChannelInfo.Ped - WaveData[i])
-#    # Make mark
-#    print("Processing entry {0}, Progress {1}%".format(k * LoadingPeriod, k * LoadingPeriod / Total_entries * 100))
-#
-#    if len(EventData) != len(Timeline) :
-#        Timeline = torch.arange(WindowSize, device=device).repeat([len(EventData), 1])
-#
-#    if k == 0 :
-#        if device != 'cpu' :
-#            # finish loading to GPU, give tag on .bulletin.swp
-#            os.system("echo {} {} >> .bulletin.swp".format(fileno, 0))
-#
-#    # calculating
-#    Prediction = net(inputs).data
-#    # checking for no pe event
-#
-#    # Makeing Output and write submission file
-#    for i in range(len(PETimes)) :
-#        answer['PETime'] = PETimes[i]
-#        answer['Weight'] = Weights[i]
-#        answer['EventID'] = EventData[i]
-#        answer['ChannelID'] = ChanData[i]
-#        answer.append()
-#
-#    # Flush into the output file
-#    AnswerTable.flush()
-#
-#
-# h5file.close()
+
+class AnswerData(tables.IsDescription):
+    EventID = tables.Int64Col(pos=0)
+    ChannelID = tables.Int16Col(pos=1)
+    PETime = tables.Int16Col(pos=2)
+    Weight = tables.Float32Col(pos=3)
+
+
+AnswerFile = tables.open_file(output, mode="w", title="OneTonDetector", filters=tables.Filters(complevel=4))
+AnswerTable = AnswerFile.create_table("/", "Answer", AnswerData, "Answer")
+AnswerTable.append([Result[name].to_numpy() for name in AnswerTable.colnames])
+AnswerTable.flush()
+AnswerFile.close()
 RawDataFile.close()
-#end_time = time.time()
-# print("Prediction_Generated")
-#
-# toc = end_time - start_time  # ~1200s 20min
-#print("Time of Computing", toc)
+print("Finished! Consuming {:.2f}s in total.".format(time.time() - global_start))
