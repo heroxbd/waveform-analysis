1832d819ca2bab9b6430145cdda7fca5c7605e2c
xudacheng <xudachengthu@hotmail.com>
2020-06-20 15:53:30 +0000

    Manage specified files in PreProcess

--- test/tara/Data_Pre-Processing.py
+++ test/tara/Data_Pre-Processing.py
@@ -3,15 +3,13 @@ import argparse
 psr = argparse.ArgumentParser()
 psr.add_argument('ipt', help='input file prefix')
 psr.add_argument('-o', '--outputdir', dest='opt', help='output_dir')
-psr.add_argument('-N', '--NWaves', dest='Nwav', type=int, help='entries of waves')
+psr.add_argument('-N', '--NFiles', dest='NFiles', type=int, nargs='+', help='entries of files')
 psr.add_argument('-f', '--format', dest='format', default='h5', choices=['h5', 'root'], help='input file format')
 args = psr.parse_args()
-Nwav = args.Nwav
+FileNo = args.NFiles
 prefix = args.ipt
 suffix = args.format
 SavePath = args.opt
-if(Nwav <= 100):
-    raise ValueError("NWaves must > 100 !")
 
 from time import time
 start = time()
@@ -36,8 +34,9 @@ def Make_Time_Vector(GroundTruth, Waveforms_and_info) :
     Wave_EventID = Waveforms_and_info["EventID"].to_numpy()
     Truth_EventID = GroundTruth["EventID"].to_numpy()
     PETime = GroundTruth["PETime"].to_numpy()
+    nt = len(Truth_EventID)
     for j in range(len(Waveforms_and_info)) :
-        while(Wave_EventID[j] == Truth_EventID[i]) :
+        while i < nt and Wave_EventID[j] == Truth_EventID[i] :
             Time_Series[j][PETime[i]] = Time_Series[j][PETime[i]] + 1
             i = i + 1
     return Time_Series
@@ -53,9 +52,6 @@ def AssignStream(WindowSize) :
     return stream
 
 
-# Make Directory
-if not os.path.exists(SavePath):
-    os.makedirs(SavePath)
 print("training data pre-processing savepath is {}".format(SavePath))
 print("Initialization Finished, consuming {:.5f}s.".format(time() - start))
 
@@ -86,36 +82,26 @@ def Read_Data(sliceNo, filename, Wave_startentry, Wave_endentry, Truth_startentr
     return (sliceNo, {"Waveform": TableToDataFrame(Waveforms_and_info), "GroundTruth": TableToDataFrame(GroundTruth)})
 
 
-FileNo = 0
 sliceNo = 0
 trainfile_list = []  # index sliceNo; value: number of waveforms to be readed
 start = time()
-while(Nwav != 0) :
-    filename = prefix + "{}".format(FileNo) + ".h5"
+for iFile in FileNo :
+    filename = prefix + "{}".format(iFile) + ".h5"
     try :
         h5file = tables.open_file(filename, "r")
     except OSError :
         raise ValueError("You demand too many training waves while providing insufficient input files!\nCheck if your taining files have continuous file numbers starting with 0.")
     # Reading file
-    if FileNo == 0 :
+    if iFile == FileNo[0]:
         origin_dtype = h5file.root.Waveform.dtype
         WindowSize = origin_dtype["Waveform"].shape[0]
-        gpufloat_dtype = np.dtype([(name, np.dtype('float32') if name == "Waveform" else origin_dtype[name].base, origin_dtype[name].shape) for name in origin_dtype.names])
     Waveform_Len = len(h5file.root.Waveform)
     GroundTruth_Len = len(h5file.root.GroundTruth)
-    if Waveform_Len > Nwav :
-        Waveform_Len = Nwav
-        GroundTruth_Len = min(round(GroundTruth_Len / len(h5file.root.Waveform) * Waveform_Len * 2), GroundTruth_Len)
-    elif Waveform_Len == Nwav :  # in case that the end of the last file is read, resulting in Index out of range when calling Make_Time_Vector
-        Nwav = Nwav - 1
-        Waveform_Len = Nwav
-    Nwav = Nwav - Waveform_Len
     slices = np.append(np.arange(0, Waveform_Len, 150000), Waveform_Len)
     Truth_slices = (GroundTruth_Len / Waveform_Len * slices).astype(np.int)
     for i in range(len(slices) - 1) :
         trainfile_list.append((sliceNo, filename, slices[i], slices[i + 1], Truth_slices[i], Truth_slices[i + 1]))
         sliceNo = sliceNo + 1
-    FileNo = FileNo + 1
     h5file.close()
 with Pool(min(len(trainfile_list), cpu_count())) as pool :
     Reading_Result = dict(pool.starmap(Read_Data, trainfile_list))
@@ -142,7 +128,7 @@ CAL_PED = np.vectorize(Calculate_Ped, signature="(n)->()", excluded={"stream"})
 
 
 def PreProcess(channelid) :
-    print("PreProcessing channel {}".format(channelid))
+    print("PreProcessing channel {:02d}".format(channelid))
     Waves_of_this_channel = Grouped_Waves.get_group(channelid)
     Truth_of_this_channel = Grouped_Truth.get_group(channelid)
     Origin_Waves = np.vstack(Waves_of_this_channel["Waveform"].to_numpy())
@@ -155,7 +141,7 @@ def PreProcess(channelid) :
     HitSpectrum = Make_Time_Vector(Truth_of_this_channel, Waves_of_this_channel)
 
     # create Pre-Processed output file
-    Prefile = tables.open_file(SavePath + "/Pre_Channel{}.h5".format(channelid), mode="w", title="Pre-Processed-Training-Data")
+    Prefile = tables.open_file(SavePath + "/Pre_Channel{:02d}.h5".format(channelid), mode="w", title="Pre-Processed-Training-Data")
     # Create group and tables
     TrainDataTable = Prefile.create_table("/", "TrainDataTable", PreProcessedData, "Wave and HitSpectrum")
     tablerow = TrainDataTable.row
