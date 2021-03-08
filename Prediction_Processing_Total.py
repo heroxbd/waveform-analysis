f772d1c6097b3262c54d8e15462abdce5dc41980
WuYiyang <xgreenlandforwyy@gmail.com>
2021-03-08 20:08:17 +0800

    DATA: process single file

--- Prediction_Processing_Total.py
+++ Prediction_Processing_Total.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # memory tracking
 
@@ -36,9 +37,6 @@ import awkward as ak
 import torch
 torch.no_grad()
 
-from multiprocessing import Pool, cpu_count
-
-
 if torch.cuda.is_available() and Device == 'cpu':
     print("WARNING: You have a CUDA device, so you should probably run with --cuda")
     Device = 'cpu'
@@ -51,11 +49,6 @@ else :
 Device = torch.device(Device)
 
 
-import os
-import re
-filenames = os.listdir(rawfilename)
-pattern = re.compile(r"(\d+)\.root")
-filenos = [int(pattern.match(fn)[1]) for fn in filenames]
 print('Initialization finished, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - global_start, time.process_time() - cpu_global_start))
 
 # Loading CNN Net
@@ -73,9 +66,7 @@ def ReadBaseline(filename) :
     return Ped
 
 
-def Read_Data(fileno) :
-    raw_filename = rawfilename + "/{}.root".format(fileno)
-    bsl_filename = bslfilename + "/{}.root".format(fileno)
+def Read_Data(raw_filename, bsl_filename, fileno) :
     RawDataFile = uproot.open(raw_filename)
     ChannelId = RawDataFile["Readout"]["ChannelId"].array()
     TriggerNo = RawDataFile["Readout"]["TriggerNo"].array()
@@ -102,105 +93,89 @@ def Read_Data(fileno) :
     return pd.DataFrame(wave_dict)
 
 
-def WriteData(fileno, Grouped_Result) :
-    optpath = output + "/{}.h5".format(fileno)
-    Result = Grouped_Result.get_group(fileno)
+def WriteData(Result, optpath) :
     Result = Result.to_records(index=False)
     with h5py.File(optpath, 'w') as opt:
-        dset = opt.create_dataset('AnswerWF', data=Result, compression='gzip')
+        dset = opt.create_dataset('AnswerWF', data=Result, compression=32015)
         dset.attrs['Method'] = method
 
 
 GainTable = np.loadtxt(gaintablefile, skiprows=0, usecols=2)
-N = min(3, cpu_count())
-SLICES = np.append(np.arange(0, len(filenos), N), len(filenos))
 tic = time.time()
 cpu_tic = time.process_time()
-with Pool(N) as pool :
-    Waveforms_and_info = pd.concat(pool.map(Read_Data, filenos[SLICES[0]:SLICES[1]]))
+fileno = int(rawfilename.split("/")[-1][:-5])
+N = 1
+Waveforms_and_info = Read_Data(rawfilename, bslfilename, fileno)
 print('Data Loaded, consuming {0:.4f}s using {1} threads, cpu time {2:.4f}s'.format(time.time() - tic, N, time.process_time() - cpu_tic))
-for i in range(len(SLICES) - 1):
-    FILENOS = filenos[SLICES[i]:SLICES[i + 1]]
-    with Pool(N) as pool_read, Pool(N) as pool_write :
-        if i < (len(SLICES) - 2) :
-            NEXT_FILENOS = filenos[SLICES[i + 1]:SLICES[i + 2]]
-            WaveReader = pool_read.map_async(Read_Data, NEXT_FILENOS)
 
-        WindowSize = len(Waveforms_and_info["Waveform"][0:1][0])
-        Channel_Grouped_Waveform = Waveforms_and_info.groupby(by='ChannelID')
+WindowSize = len(Waveforms_and_info["Waveform"][0:1][0])
+Channel_Grouped_Waveform = Waveforms_and_info.groupby(by='ChannelID')
 
-        filter_limit = 0.05
-        Timeline = np.arange(WindowSize, dtype=np.int16).reshape(1, WindowSize)
+filter_limit = 0.05
+Timeline = np.arange(WindowSize, dtype=np.int16).reshape(1, WindowSize)
 
-        def Forward(channelid) :
-            SPECharge = GainTable[channelid]
-            filter_limit = 0.01 * SPECharge
-            try :
-                Data_of_this_channel = Channel_Grouped_Waveform.get_group(channelid)
-            except KeyError :
-                print("Warning: channel {0} not found in file {1}!".format(channelid, FILENOS))
-                return None
-            Wave = np.vstack(Data_of_this_channel['Waveform'])
-            Peds = np.array(Data_of_this_channel['Pedestal'])[:, None]
-            TriggerNos = np.array(Data_of_this_channel['TriggerNo'])
-            FileNos = np.array(Data_of_this_channel['FileNo'])
-            HitPosInWindows = np.empty(0, dtype=np.int16)
-            PEmeasure = np.empty(0, dtype=np.float64)
-            EventData = np.empty(0, dtype=np.uint32)
-            FileNoData = np.empty(0, dtype=np.int32)
-            slices = np.append(np.arange(0, len(Wave), BATCHSIZE), len(Wave))
-            for i in range(len(slices) - 1) :
-                inputs = Peds[slices[i]:slices[i + 1]] - Wave[slices[i]:slices[i + 1]]
-                Total = np.abs(np.sum(inputs, axis=1))
-                Total = np.where(Total > 1e-4, Total, 1e-4)
-                if Device != 'cpu' :
-                    Prediction = nets[channelid].forward(torch.from_numpy(inputs).to(Device)).data.cpu().numpy()
-                else :
-                    Prediction = nets[channelid].forward(torch.from_numpy(inputs)).data.numpy()
-                sumPrediction = np.sum(Prediction, axis=1)
-                sumPrediction = np.where(sumPrediction > 1e-4, sumPrediction, 1e-4)
-                HitPosInWindow = Prediction > filter_limit
-                Prediction = Prediction / sumPrediction[:, None]
-                pe_numbers = HitPosInWindow.sum(axis=1)
-                no_pe_found = pe_numbers == 0
-                if no_pe_found.any() :
-                    guessed_risetime = inputs[no_pe_found].argmax(axis=1) - 2  # arbitary parameter :2
-                    HitPosInWindow[no_pe_found, guessed_risetime] = True
-                    Prediction[no_pe_found, guessed_risetime] = 1
-                    pe_numbers[no_pe_found] = 1
-                Prediction = Prediction / np.sum(Prediction, axis=1)[:, None] * Total[:, None]
-                Prediction = Prediction[HitPosInWindow]
-                PEmeasure = np.append(PEmeasure, Prediction)
-                TimeMatrix = np.repeat(Timeline, len(HitPosInWindow), axis=0)[HitPosInWindow]
-                HitPosInWindows = np.append(HitPosInWindows, TimeMatrix)
-                EventData = np.append(EventData, np.repeat(TriggerNos[slices[i]:slices[i + 1]], pe_numbers))
-                FileNoData = np.append(FileNoData, np.repeat(FileNos[slices[i]:slices[i + 1]], pe_numbers))
-                ChannelData = np.empty(EventData.shape, dtype=np.uint32)
-                ChannelData.fill(channelid)
-            return pd.DataFrame({'HitPosInWindow': HitPosInWindows, 'Charge': PEmeasure, 'TriggerNo': EventData, 'ChannelID': ChannelData, 'FileNo': FileNoData})
 
-        tic = time.time()
-        cpu_tic = time.process_time()
-        Result = []
-        for ch in tqdm(range(30), desc='Predict for each channel') :
-            Result.append(Forward(ch))
-        Result = pd.concat(Result)
-        Result = Result.sort_values(by=['TriggerNo', 'ChannelID'])
-        Grouped_Result = Result.groupby(by='FileNo')
-        print('Prediction generated, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - tic, time.process_time() - cpu_tic))
-        os.system("rm -rf /tmp/miopen-*")
+def Forward(channelid) :
+    SPECharge = GainTable[channelid]
+    filter_limit = 0.01 * SPECharge
+    try :
+        Data_of_this_channel = Channel_Grouped_Waveform.get_group(channelid)
+    except KeyError :
+        print("Warning: channel {0} not found in file {1}!".format(channelid, fileno))
+        return None
+    Wave = np.vstack(Data_of_this_channel['Waveform'])
+    Peds = np.array(Data_of_this_channel['Pedestal'])[:, None]
+    TriggerNos = np.array(Data_of_this_channel['TriggerNo'])
+    FileNos = np.array(Data_of_this_channel['FileNo'])
+    HitPosInWindows = np.empty(0, dtype=np.int16)
+    PEmeasure = np.empty(0, dtype=np.float64)
+    EventData = np.empty(0, dtype=np.uint32)
+    FileNoData = np.empty(0, dtype=np.int32)
+    slices = np.append(np.arange(0, len(Wave), BATCHSIZE), len(Wave))
+    for i in range(len(slices) - 1) :
+        inputs = Peds[slices[i]:slices[i + 1]] - Wave[slices[i]:slices[i + 1]]
+        Total = np.abs(np.sum(inputs, axis=1))
+        Total = np.where(Total > 1e-4, Total, 1e-4)
+        if Device != 'cpu' :
+            Prediction = nets[channelid].forward(torch.from_numpy(inputs).to(Device)).data.cpu().numpy()
+        else :
+            Prediction = nets[channelid].forward(torch.from_numpy(inputs)).data.numpy()
+        sumPrediction = np.sum(Prediction, axis=1)
+        sumPrediction = np.where(sumPrediction > 1e-4, sumPrediction, 1e-4)
+        HitPosInWindow = Prediction > filter_limit
+        Prediction = Prediction / sumPrediction[:, None]
+        pe_numbers = HitPosInWindow.sum(axis=1)
+        no_pe_found = pe_numbers == 0
+        if no_pe_found.any() :
+            guessed_risetime = inputs[no_pe_found].argmax(axis=1) - 2  # arbitary parameter :2
+            HitPosInWindow[no_pe_found, guessed_risetime] = True
+            Prediction[no_pe_found, guessed_risetime] = 1
+            pe_numbers[no_pe_found] = 1
+        Prediction = Prediction / np.sum(Prediction, axis=1)[:, None] * Total[:, None]
+        Prediction = Prediction[HitPosInWindow]
+        PEmeasure = np.append(PEmeasure, Prediction)
+        TimeMatrix = np.repeat(Timeline, len(HitPosInWindow), axis=0)[HitPosInWindow]
+        HitPosInWindows = np.append(HitPosInWindows, TimeMatrix)
+        EventData = np.append(EventData, np.repeat(TriggerNos[slices[i]:slices[i + 1]], pe_numbers))
+        FileNoData = np.append(FileNoData, np.repeat(FileNos[slices[i]:slices[i + 1]], pe_numbers))
+        ChannelData = np.empty(EventData.shape, dtype=np.uint32)
+        ChannelData.fill(channelid)
+    return pd.DataFrame({'HitPosInWindow': HitPosInWindows, 'Charge': PEmeasure, 'TriggerNo': EventData, 'ChannelID': ChannelData, 'FileNo': FileNoData})
 
-        tic = time.time()
-        cpu_tic = time.process_time()
-        pool_write.starmap(WriteData, [(fn, Grouped_Result) for fn in FILENOS])
-        print('output written, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - tic, time.process_time() - cpu_tic))
 
-        if i < (len(SLICES) - 2) :
-            tic = time.time()
-            cpu_tic = time.process_time()
-            WaveReader.wait()
-            Waveforms_and_info = pd.concat(WaveReader.get())
-            print('reading the next inputs, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - tic, time.process_time() - cpu_tic))
+tic = time.time()
+cpu_tic = time.process_time()
+Result = []
+for ch in tqdm(range(30), desc='Predict for each channel') :
+    Result.append(Forward(ch))
+Result = pd.concat(Result)
+Result = Result.sort_values(by=['TriggerNo', 'ChannelID'])
+print('Prediction generated, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - tic, time.process_time() - cpu_tic))
+
+tic = time.time()
+cpu_tic = time.process_time()
+WriteData(Result, output)
+print('output written, real time {0:.4f}s, cpu time {1:.4f}s'.format(time.time() - tic, time.process_time() - cpu_tic))
 
 
 print('Finished! Consuming {0:.2f}s in total, cpu time {1:.2f}s.'.format(time.time() - global_start, time.process_time() - cpu_global_start))
