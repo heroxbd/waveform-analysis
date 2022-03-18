ba302ff2e69d048793e61b46b1ccee2fe6c88bc1
Berrysoft <Strawberry_Str@hotmail.com>
2022-03-18 20:42:28 +0800

    Format tensorbatch

--- tensorbatch.py
+++ tensorbatch.py
@@ -33,10 +33,12 @@ def vcombine(A, cx, t):
     ti = tf.cast(tf.floor(t), tf.int32)
     A = tf.transpose(A, perm=(0, 2, 1))
     cx = tf.transpose(cx, perm=(0, 2, 1))
-    A_vec = (1 - frac)[:, :, None] * tf.gather(A, ti, batch_dims=1) + \
-        frac[:, :, None] * tf.gather(A, ti+1, batch_dims=1)
-    c_vec = (1 - frac)[:, :, None] * tf.gather(cx, ti, batch_dims=1) + \
-        frac[:, :, None] * tf.gather(cx, ti+1, batch_dims=1)
+    A_vec = (1 - frac)[:, :, None] * tf.gather(A, ti, batch_dims=1) + frac[
+        :, :, None
+    ] * tf.gather(A, ti + 1, batch_dims=1)
+    c_vec = (1 - frac)[:, :, None] * tf.gather(cx, ti, batch_dims=1) + frac[
+        :, :, None
+    ] * tf.gather(cx, ti + 1, batch_dims=1)
     return A_vec, c_vec
 
 
@@ -50,12 +52,12 @@ def vmove1(A_vec, c_vec, z, fmu, fsig2s_inv, det_fsig2s_inv, b0):
     beta = tf.linalg.inv(beta_inv)
     # beta = tf.constant(cp.asnumpy(cp.linalg.inv(cp.array(beta_inv))))
 
-    zc = tf.squeeze(c_vec @ z[:, :, None]) + \
-        tf.squeeze(fmu[:, None, :] @ fsig2s_inv)
+    zc = tf.squeeze(c_vec @ z[:, :, None]) + tf.squeeze(fmu[:, None, :] @ fsig2s_inv)
     Δν = 0.5 * tf.squeeze(zc[:, None, :] @ beta @ zc[:, :, None])
 
-    Δν += 0.5 * tf.math.log(tf.clip_by_value(tf.linalg.det(beta)
-                            * det_fsig2s_inv, 1/b0, b0))
+    Δν += 0.5 * tf.math.log(
+        tf.clip_by_value(tf.linalg.det(beta) * det_fsig2s_inv, 1 / b0, b0)
+    )
     return Δν, beta
 
 
@@ -78,7 +80,7 @@ def periodic(_h, _lt):
 def v_rt(_s, _ts, w_all):
     frac, ti = np.modf(_s)
     ti = np.array(ti, np.int32)
-    return (1 - frac) * _ts[w_all, ti] + frac * _ts[w_all, ti+1]
+    return (1 - frac) * _ts[w_all, ti] + frac * _ts[w_all, ti + 1]
 
 
 tau = 20
@@ -91,7 +93,7 @@ s2s = np.sqrt(2.0) * sigma
 
 # tf.function(jit_compile=True)
 def lc(t):
-    return co + tf.math.log(1.0 - erf((ass - t)/s2s)) - alpha*t
+    return co + tf.math.log(1.0 - erf((ass - t) / s2s)) - alpha * t
 
 
 # tf.function(jit_compile=True)
@@ -112,8 +114,12 @@ def batch(A, cx, index, tq, s, z):
 
     # 根据 p_cha 采样得到的 PE 序列。供以后的产生过程使用。这两行是使用了 InverseCDF 算法进行的MC采样。
     fp = np.arange(l_t)
-    home_s = np.array([np.interp(_is, xp=_xp[:_lt], fp=fp[:_lt])
-                      for _is, _xp, _lt in zip(istar, tq["cq"], index["l_t"])])
+    home_s = np.array(
+        [
+            np.interp(_is, xp=_xp[:_lt], fp=fp[:_lt])
+            for _is, _xp, _lt in zip(istar, tq["cq"], index["l_t"])
+        ]
+    )
 
     t0 = np.zeros(l_e, np.float32)
     e_hit = NPE > 0
@@ -123,8 +129,7 @@ def batch(A, cx, index, tq, s, z):
     t0[e_nonhit] = tq["t_s"][e_nonhit, 0]
 
     # step: +1 创生一个 PE， -1 消灭一个 PE， +2 向左或向右移动
-    flip = np.random.choice((-1, 1, 2), (l_e, TRIALS),
-                            p=np.array((1, 1, 2)) / 4)
+    flip = np.random.choice((-1, 1, 2), (l_e, TRIALS), p=np.array((1, 1, 2)) / 4)
     Δν_g = tf.zeros(l_e, dtype=np.float32)
     Δν = np.zeros(l_e, dtype=np.float32)
     Δν_history = np.zeros((l_e, TRIALS), dtype=np.float32)  # list of Δν's
@@ -135,7 +140,7 @@ def batch(A, cx, index, tq, s, z):
 
     log_mu = np.log(index["mu0"])  # 猜测的 Poisson 流强度
     loc = np.zeros((l_e, 2))  # float64
-    fsig2s_inv = tf.linalg.diag(vstep)[None, :, :] * (1/sig2s)[:, None, None]
+    fsig2s_inv = tf.linalg.diag(vstep)[None, :, :] * (1 / sig2s)[:, None, None]
     det_fsig2s_inv = tf.linalg.det(fsig2s_inv)
     fmu = vstep[None, :] * mus[:, None]
 
@@ -149,16 +154,14 @@ def batch(A, cx, index, tq, s, z):
         )
     ):
         if i % 1000 == 0:
-            print(i, NPE)
+            print(i)
 
         mNPE = np.max(NPE)
         rt = v_rt(s[:, :mNPE], tq["t_s"], np.arange(l_e)[:, None])
         nt0 = t0 + wt.astype(np.float32)
         sel = np.arange(mNPE)[None, :] < NPE[:, None]
-        lc0 = tf.reduce_sum(
-            tf.where(sel, lc(rt - t0[:, None]), 0), axis=1)
-        lc1 = tf.reduce_sum(
-            tf.where(sel, lc(rt - nt0[:, None]), 0), axis=1)
+        lc0 = tf.reduce_sum(tf.where(sel, lc(rt - t0[:, None]), 0), axis=1)
+        lc1 = tf.reduce_sum(tf.where(sel, lc(rt - nt0[:, None]), 0), axis=1)
         np.putmask(t0, (lc1 - lc0).numpy() >= acct, nt0)
         t0_history[:, i] = t0
 
@@ -179,8 +182,7 @@ def batch(A, cx, index, tq, s, z):
         loc[e_create, 0] = l_t  # 0 A_vec
         op = np.array(t * NPE, dtype=np.int32)
         loc[e_minus, 0] = s[e_minus, op[e_minus]]  # annihilate + move
-        loc[e_move, 1] = periodic(
-            loc[e_move, 0] + wander[e_move], index["l_t"][e_move])
+        loc[e_move, 1] = periodic(loc[e_move, 0] + wander[e_move], index["l_t"][e_move])
         loc[e_create, 1] = periodic(home[e_create], index["l_t"][e_create])
         loc[e_annihilate, 1] = l_t
 
@@ -189,15 +191,17 @@ def batch(A, cx, index, tq, s, z):
         Δν_g, beta = vmove1(vA, vc, z, fmu, fsig2s_inv, det_fsig2s_inv, b0)
 
         # -1 cases, step == 2, -1
-        Δν[e_minus] -= lc(v_rt(loc[e_minus, 0], tq["t_s"],
-                          e_minus) - t0[e_minus])
+        Δν[e_minus] -= lc(v_rt(loc[e_minus, 0], tq["t_s"], e_minus) - t0[e_minus])
         # +1 cases, step == 2, 1
         Δν[e_plus] += lc(v_rt(loc[e_plus, 1], tq["t_s"], e_plus) - t0[e_plus])
         # non-move cases, step == 1, -1
         NPE[e_create] += 1
         loc[e_annihilate, 1] = loc[e_annihilate, 0]
-        Δν[e_pm] += step[e_pm] * (log_mu[e_pm] - np.log(
-            tq["q_s"][e_pm, np.array(loc[e_pm, 1], dtype=np.int32) + 1]) - np.log(NPE[e_pm]))
+        Δν[e_pm] += step[e_pm] * (
+            log_mu[e_pm]
+            - np.log(tq["q_s"][e_pm, np.array(loc[e_pm, 1], dtype=np.int32) + 1])
+            - np.log(NPE[e_pm])
+        )
         loc[e_annihilate, 1] = l_t
         NPE[e_create] -= 1
         ########
@@ -249,16 +253,24 @@ with h5py.File(fipt, "r", libver="latest", swmr=True) as ipt:
 l_e = len(index)
 s_t = np.argsort(index["l_t"])
 
-sample = np.zeros((l_e * TRIALS), dtype=[("TriggerNo", "u4"), ("ChannelID", "u4"),
-                                         ("flip", "i2"), ("s0",
-                                                          "u4"), ("t0", "f8"),
-                                         ("annihilation", "f8"), ("creation", "f8"),
-                                         ("delta_nu", "f4")])
+sample = np.zeros(
+    (l_e * TRIALS),
+    dtype=[
+        ("TriggerNo", "u4"),
+        ("ChannelID", "u4"),
+        ("flip", "i2"),
+        ("s0", "u4"),
+        ("t0", "f8"),
+        ("annihilation", "f8"),
+        ("creation", "f8"),
+        ("delta_nu", "f4"),
+    ],
+)
 sample["TriggerNo"] = np.repeat(index["TriggerNo"], TRIALS)
 sample["ChannelID"] = np.repeat(index["ChannelID"], TRIALS)
 
 for part in range(l_e // args.size + 1):
-    i_part = s_t[part * args.size:(part + 1) * args.size]
+    i_part = s_t[part * args.size : (part + 1) * args.size]
     l_part = len(i_part)
     if l_part:
         ind_part = index[i_part]
@@ -271,15 +283,19 @@ for part in range(l_e // args.size + 1):
         null = np.zeros((l_part, lp_wave, 1), np.float32)
         s_null = np.zeros((l_part, lp_NPE * 2), np.float32)  # 富余的 PE 活动空间
         A_slice = np.asarray(
-            np.append(A[i_part, :lp_wave, :lp_t], null, axis=2), dtype=np.float32)
+            np.append(A[i_part, :lp_wave, :lp_t], null, axis=2), dtype=np.float32
+        )
         cx_slice = np.asarray(
-            np.append(cx[i_part, :lp_wave, :lp_t], null, axis=2), dtype=np.float32)
-        (flip, s0_history, t0_history, Δν_history, annihilations, creations,
-         ) = batch(tf.constant(A_slice, dtype=tf.float32),
-                   tf.constant(cx_slice, dtype=tf.float32),
-                   index[i_part], tq[i_part, :lp_t],
-                   np.append(s[i_part, :lp_NPE], s_null, axis=1),
-                   tf.constant(z[i_part, :lp_wave], dtype=tf.float32))
+            np.append(cx[i_part, :lp_wave, :lp_t], null, axis=2), dtype=np.float32
+        )
+        (flip, s0_history, t0_history, Δν_history, annihilations, creations) = batch(
+            tf.constant(A_slice, dtype=tf.float32),
+            tf.constant(cx_slice, dtype=tf.float32),
+            index[i_part],
+            tq[i_part, :lp_t],
+            np.append(s[i_part, :lp_NPE], s_null, axis=1),
+            tf.constant(z[i_part, :lp_wave], dtype=tf.float32),
+        )
         fi_part = (i_part * TRIALS)[:, None] + np.arange(TRIALS)[None, :]
         fip = fi_part.flatten()
         sample["flip"][fip] = flip.flatten()
